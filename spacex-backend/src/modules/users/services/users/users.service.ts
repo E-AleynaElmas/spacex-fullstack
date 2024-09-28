a2e17cb2cb7a5a548a77228eb2467c70f700bd5a
Aley <a.aleynaelmas@gmail.com>
2024-09-29 00:06:19 +0300

    feat: Implement Users module with service and controller.

--- spacex-backend/src/modules/users/services/users/users.service.ts
+++ spacex-backend/src/modules/users/services/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../../entities/user.entity';
+import { CreateUserDto } from '../../dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  create(user: CreateUserDto): Promise<User> {
+    const newUser = this.usersRepository.create(user);
+    return this.usersRepository.save(newUser);
+  }
+
+  findByEmail(email: string): Promise<User> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+}
