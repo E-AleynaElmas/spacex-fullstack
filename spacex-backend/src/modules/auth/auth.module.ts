c8fdaa94f56b207fceddd7c9ace1e399dd1622bc
Aley <a.aleynaelmas@gmail.com>
2024-09-29 00:12:18 +0300

    feat: Implement AuthModule with user registration and bcrypt password hashing.

--- spacex-backend/src/modules/auth/auth.module.ts
+++ spacex-backend/src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './services/auth/auth.service';
 import { AuthController } from './controllers/auth/auth.controller';
 
 @Module({
+  imports: [UsersModule],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
