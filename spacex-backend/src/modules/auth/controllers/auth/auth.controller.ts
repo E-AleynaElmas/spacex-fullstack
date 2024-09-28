a634f419394a55c7ce42543b4bbb90c2c93cb7fe
Aley <a.aleynaelmas@gmail.com>
2024-09-29 00:14:51 +0300

    feat: Add register endpoint to AuthController.

--- spacex-backend/src/modules/auth/controllers/auth/auth.controller.ts
+++ spacex-backend/src/modules/auth/controllers/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from '../../services/auth/auth.service';
+import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() userDto: CreateUserDto) {
+    return this.authService.register(userDto);
+  }
+
+  //TODO:  Login ve me endpointleri ekle.
+}
--- spacex-backend/src/modules/auth/services/auth/auth.service.ts
+++ spacex-backend/src/modules/auth/services/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
+import { UsersService } from 'src/modules/users/services/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async register(userDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(userDto.password, 10);
+    const user = await this.usersService.create({
+      ...userDto,
+      password: hashedPassword,
+    });
+    return user;
+  }
+
+  //TODO: Login metodu ekle.
+}
