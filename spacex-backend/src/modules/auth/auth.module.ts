10e6bd7897e4b77bdcdaa30415252cfb11d9ddcd
Aley <a.aleynaelmas@gmail.com>
2024-09-29 00:32:00 +0300

    feat: Update AuthModule with JWT and Passport integration.

--- spacex-backend/src/modules/auth/auth.module.ts
+++ spacex-backend/src/modules/auth/auth.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from '../../common/strategies/jwt.strategy';
 import { AuthService } from './services/auth/auth.service';
 import { AuthController } from './controllers/auth/auth.controller';
+import { UsersModule } from '../users/users.module';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'temp_secret_key',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
