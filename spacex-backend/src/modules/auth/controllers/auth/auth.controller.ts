3de7f9defca5b315683031115ce5a7b9827ba3fe
Aley <a.aleynaelmas@gmail.com>
2024-09-29 00:40:39 +0300

    feat: Add login and protected profile (me) endpoints to AuthController.

--- spacex-backend/src/modules/auth/controllers/auth/auth.controller.ts
+++ spacex-backend/src/modules/auth/controllers/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+  Get,
+} from '@nestjs/common';
 import { AuthService } from '../../services/auth/auth.service';
 import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
+import { LoginDto } from '../../dto/login.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +20,14 @@ export class AuthController {
     return this.authService.register(userDto);
   }
 
-  //TODO:  Login ve me endpointleri ekle.
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 }
--- spacex-backend/src/modules/auth/services/auth/auth.service.ts
+++ spacex-backend/src/modules/auth/services/auth/auth.service.ts
@@ -3,10 +3,14 @@ import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
 import { UsersService } from 'src/modules/users/services/users/users.service';
 import { LoginDto } from '../../dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(userDto: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(userDto.password, 10);
@@ -20,8 +24,10 @@ export class AuthService {
   async login(loginDto: LoginDto) {
     const user = await this.usersService.findByEmail(loginDto.email);
     if (user && (await bcrypt.compare(loginDto.password, user.password))) {
-      // Kullanıcı doğrulandı, JWT token oluşturulabilir
-      return user;
+      const payload = { sub: user.id, email: user.email };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
     } else {
       throw new Error('Invalid credentials');
     }
