fa4b699d32623c21540a5d214bfc8bb858103bae
Aley <a.aleynaelmas@gmail.com>
2024-09-28 23:42:59 +0300

    feat: Generate Users service and controller.

--- spacex-backend/src/modules/users/users.module.ts
+++ spacex-backend/src/modules/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './services/users/users.service';
+import { UsersController } from './controllers/users/users.controller';
 
-@Module({})
+@Module({
+  providers: [UsersService],
+  controllers: [UsersController]
+})
 export class UsersModule {}
