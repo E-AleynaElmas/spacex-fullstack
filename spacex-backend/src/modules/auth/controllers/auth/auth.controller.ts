216aed3bc8ac1148524a910bbd898fec84de9c7d
Aley <a.aleynaelmas@gmail.com>
2024-09-29 17:02:36 +0300

    feat: Add Swagger ApiResponse decorators to AuthController.

--- spacex-backend/src/modules/auth/controllers/auth/auth.controller.ts
+++ spacex-backend/src/modules/auth/controllers/auth/auth.controller.ts
@@ -10,23 +10,35 @@ import { AuthService } from '../../services/auth/auth.service';
 import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
 import { LoginDto } from '../../dto/login.dto';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { ApiTags, ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully registered.',
+  })
+  @ApiResponse({ status: 400, description: 'Invalid request.' })
   register(@Body() userDto: CreateUserDto) {
     return this.authService.register(userDto);
   }
 
   @Post('login')
+  @ApiResponse({ status: 200, description: 'Login successfull.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('me')
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'User profile.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   getProfile(@Request() req) {
     return req.user;
   }
