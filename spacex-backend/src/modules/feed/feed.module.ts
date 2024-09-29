0560d9faf46bf537f3c626159c2089ee023b8004
Aley <a.aleynaelmas@gmail.com>
2024-09-29 17:31:18 +0300

    feat: Configure FeedModule and add UpdateFeedDto.

--- spacex-backend/src/modules/feed/feed.module.ts
+++ spacex-backend/src/modules/feed/feed.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Feed } from './entities/feed.entity';
 import { FeedService } from './services/feed/feed.service';
 import { FeedController } from './controllers/feed/feed.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Feed])],
   providers: [FeedService],
-  controllers: [FeedController]
+  controllers: [FeedController],
 })
 export class FeedModule {}
