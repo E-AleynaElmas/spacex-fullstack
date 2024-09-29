7019b04675e9cb21d366daea1eb97860b78d614c
Aley <a.aleynaelmas@gmail.com>
2024-09-29 17:23:32 +0300

    feat: Generate Feed module.

--- spacex-backend/src/app.module.ts
+++ spacex-backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfig } from './config/database.config';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { FeedModule } from './modules/feed/feed.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(DatabaseConfig), UsersModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(DatabaseConfig), UsersModule, AuthModule, FeedModule],
 })
 export class AppModule {}
