c73323fdfb1ad03be6700b1644d35331b8a7552a
Aley <a.aleynaelmas@gmail.com>
2024-09-29 16:47:20 +0300

    feat: Add Swagger ApiProperty decorators to LoginDto and CreateUserDto.

--- spacex-backend/src/modules/auth/dto/login.dto.ts
+++ spacex-backend/src/modules/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({ description: 'User Email' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ description: 'User Password' })
   @IsNotEmpty()
   password: string;
 }
--- spacex-backend/src/modules/users/dto/create-user.dto.ts
+++ spacex-backend/src/modules/users/dto/create-user.dto.ts
@@ -1,15 +1,20 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'User Name' })
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty({ description: 'User Last Name' })
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty({ description: 'User Email' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ description: 'User Password', minLength: 6 })
   @MinLength(6)
   password: string;
 }
