f637871dbb6e0b3c00135877100d0be2cf81c5af
Aley <a.aleynaelmas@gmail.com>
2024-09-30 21:51:03 +0300

    feat: Implement CRUD operations in EventsService.

--- spacex-backend/src/modules/feed/feed.module.ts
+++ spacex-backend/src/modules/feed/feed.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Feed } from './entities/feed.entity';
 import { FeedService } from './services/feed/feed.service';
 import { FeedController } from './controllers/feed/feed.controller';
+import { EventService } from './services/event/event.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Feed])],
-  providers: [FeedService],
+  providers: [FeedService, EventService],
   controllers: [FeedController],
 })
 export class FeedModule {}
